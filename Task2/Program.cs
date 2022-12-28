b9bb317d58d6b2c438e4f15a3c110f4c24644785
Volodya <volodya.atoyan@list.ru>
2022-12-28 12:35:57 +0300

    added task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите координату X первой точки: ");
+int.TryParse(ReadLine(), out int ax);
+Write("Введите координату Y первой точки: ");
+int.TryParse(ReadLine(), out int ay);
+Write("Введите координату Z первой точки: ");
+int.TryParse(ReadLine(), out int az);
+Write("Введите координату X второй точки: ");
+int.TryParse(ReadLine(), out int bx);
+Write("Введите координату Y второй точки: ");
+int.TryParse(ReadLine(), out int by);
+Write("Введите координату Z второй точки: ");
+int.TryParse(ReadLine(), out int bz);
+
+int A = bx - ax;
+int B = by - ay;
+int C = az - bz;
+
+double length = Math.Sqrt(A * A + B * B + C * C);
+WriteLine($"Длинна отрезка {length:f2}");
