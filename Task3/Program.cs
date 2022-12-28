911177ea0c1f7e0767f73577e88d0920d16ae903
Volodya <volodya.atoyan@list.ru>
2022-12-28 12:37:43 +0300

    added task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите число N: ");
+int a = Convert.ToInt32(ReadLine());
+int i = 1;
+
+while(i <= a)
+{
+    Write($"{i*i*i} ");
+    i++;
+}
